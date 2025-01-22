59033a792b12c401b8e942726cc1752848b72839
Rushdy Naloordeen <rushdynaloordeen@gmail.com>
2025-01-22 22:54:55 +0530

    Add-some-notes-to-useEffect-2025-01-22

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 
 function App() {
  
-
   const [status,setStatus] = React.useState(false)
-
   
-
+  //NOTE:: This will render in every render 
   React.useEffect(()=> {
-    console.log("Renderd")
+    console.log("On every componant render!====== 1")
+  })
+  
+  //NOTE:: This will render in first mount/load
+  React.useEffect(()=> {
+    console.log("On first mount/load componant render!=====2")
+  }, [])
+
+  //NOTE:: This will render in first and state chnaged
+  React.useEffect(()=> {
+    console.log("On first mount/load componant render and dep render!=======3")
   }, [status])
  
   function clickHandler() { 
